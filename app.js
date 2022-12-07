5ba3203a6e840d7caf855852775a4420ad361eff
Mark Vasilkov <mvasilkov@gmail.com>
2022-12-07 10:57:31 +0200

    Check the number of references

--- app.js
+++ app.js
@@ -29,18 +29,33 @@ function main() {
         traverse(ast, {
             VariableDeclaration(path) {
                 const decl = path.node
+                let opt
                 switch (false) {
                     case decl.trailingComments?.[0]?.type === 'CommentLine':
-                    case decl.trailingComments?.[0]?.value === ' Inline':
+                    case (opt = decl.trailingComments?.[0]?.value?.match(/^ Inline(?:\((\d+)\))?$/)) ?? false:
                     case decl.kind === 'const':
                     case decl.declarations.length === 1:
                         return
                 }
+                const N = opt[1] ? parseInt(opt[1]) : 1
+                console.log(`Michikoid found Inline(${N})`)
+                console.log(generate(decl, { comments: false }, js).code)
+
+                const { name } = decl.declarations[0].id
+                const binding = path.scope.getBinding(name)
+                if (!binding.referenced) {
+                    console.log('Not referenced, skipping')
+                    return
+                }
+                else if (binding.references !== N) {
+                    console.log(`Want ${N} references, got ${binding.references} instead, skipping`)
+                    return
+                }
             },
         })
-        const result = generate(ast, {}, js)
+        const result = generate(ast, {}, js).code
 
-        fs.writeFileSync(a, result.code, 'utf-8')
+        fs.writeFileSync(a, result, 'utf-8')
     })
 }
 
