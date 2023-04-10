66cef8f87a688fa0166eb0bbf127722101a86ac4
Mark Vasilkov <mvasilkov@gmail.com>
2023-04-11 00:23:10 +0300

    Update

--- test/InlineExp.js
+++ test/InlineExp.js
@@ -3,39 +3,66 @@ import test from 'node:test'
 import { expandMacros } from '../app.js'
 
 const progIn1 = `
-let energy = null;
-let seam = null;
-for (let n = 0; n < count; ++n) {
-  energy = calculateEnergyMap(img, size); // .InlineExp
-  seam = findLowEnergySeam(energy, size);
-  deleteSeam(img, seam, size);
+export class Constraint {
+  constructor(v0, v1) {
+    this.v0 = v0
+    this.v1 = v1
+    this.p0 = v0.position
+    this.p1 = v1.position // .InlineExp
+    this.lengthSquared = this.p0.distanceSquared(this.p1)
+  }
 }`.replace('\n', '')
 
 const progOut1 = `
-let energy = null;
-let seam = null;
-for (let n = 0; n < count; ++n) {
-  seam = findLowEnergySeam(energy = calculateEnergyMap(img, size), size);
-  deleteSeam(img, seam, size);
+export class Constraint {
+  constructor(v0, v1) {
+    this.v0 = v0;
+    this.v1 = v1;
+    this.p0 = v0.position;
+    this.lengthSquared = this.p0.distanceSquared(this.p1 = v1.position);
+  }
 }`.replace('\n', '')
 
 const progIn2 = `
-let energy = null;
-let seam = null;
-for (let n = 0; n < count; ++n) {
-  energy = calculateEnergyMap(img, size); // .InlineExp
-  seam = findLowEnergySeam(energy, size); // .InlineExp
-  deleteSeam(img, seam, size);
+export class Constraint {
+  constructor(v0, v1) {
+    this.v0 = v0
+    this.v1 = v1
+    this.p0 = v0.position // .InlineExp
+    this.p1 = v1.position // .InlineExp
+    this.lengthSquared = this.p0.distanceSquared(this.p1)
+  }
 }`.replace('\n', '')
 
 const progOut2 = `
-let energy = null;
-let seam = null;
-for (let n = 0; n < count; ++n) {
-  deleteSeam(img, seam = findLowEnergySeam(energy = calculateEnergyMap(img, size), size), size);
+export class Constraint {
+  constructor(v0, v1) {
+    this.v0 = v0;
+    this.v1 = v1;
+    this.lengthSquared = (this.p0 = v0.position).distanceSquared(this.p1 = v1.position);
+  }
+}`.replace('\n', '')
+
+const progIn3 = `
+export class Constraint {
+  constructor(v0, v1) {
+    this.v0 = v0 // .InlineExp
+    this.v1 = v1 // .InlineExp
+    this.p0 = this.v0.position // .InlineExp
+    this.p1 = this.v1.position // .InlineExp
+    this.lengthSquared = this.p0.distanceSquared(this.p1)
+  }
+}`.replace('\n', '')
+
+const progOut3 = `
+export class Constraint {
+  constructor(v0, v1) {
+    this.lengthSquared = (this.p0 = (this.v0 = v0).position).distanceSquared(this.p1 = (this.v1 = v1).position);
+  }
 }`.replace('\n', '')
 
 test('InlineExp', () => {
-    assert.strictEqual(expandMacros(progIn1), progOut1)
-    assert.strictEqual(expandMacros(progIn2), progOut2)
+  assert.strictEqual(expandMacros(progIn1), progOut1)
+  assert.strictEqual(expandMacros(progIn2), progOut2)
+  assert.strictEqual(expandMacros(progIn3), progOut3)
 })
